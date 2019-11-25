9c3a9d12dd7e752fba4d68001f2b3ba61358489f
Arife Ljutviovska <38080549+Arifee@users.noreply.github.com>
2019-11-25 13:44:23 +0100

    Add files via upload

--- src/main/java/org/springframework/gsspringboot/GsSpringBootApplication.java
+++ src/main/java/org/springframework/gsspringboot/GsSpringBootApplication.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GsSpringBootApplication {
 
-    @RequestMapping("/")
+    @RequestMapping("/helloWorld")
     public String home() {
         return "Hello from Spring Boot!";
     }
