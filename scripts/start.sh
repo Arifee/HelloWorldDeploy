f742a2c92d067951e28ba2b360b77fb040cd705f
Arife Ljutviovska <38080549+Arifee@users.noreply.github.com>
2019-11-28 10:15:16 +0100

    Add files via upload

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,5 +3,5 @@
 cd /home/ec2-user/server
 sudo /usr/bin/java -jar -Dserver.port=8080 \
     *.jar > /dev/null 2> /dev/null < /dev/null &
-	mvn clean install
+	mvn clean install &
 	java -jar target/gs-spring-boot-0.0.1-SNAPSHOT.jar 
